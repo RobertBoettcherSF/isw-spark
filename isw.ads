427ca3f842968ff57d61a1c2c66f826efa54c450
Sternenfisch <105643443+RobertBoettcherSF@users.noreply.github.com>
2026-06-21 19:48:34 +0200

    Create isw.ads

---
+++ isw.ads
@@ -0,0 +1,16 @@
+package ISW is
+   -- Based on the ISW scheme requiring only the frequency vector
+   -- for an alphabet of size M and window length W.
+   type Frequency_Vector is array (Positive range <>) of Natural;
+
+   type Window_State(M : Positive; W : Natural) is record
+      Freqs : Frequency_Vector(1 .. M);
+      Total_W : Natural := W;
+   end record;
+
+   procedure Update_Window(State : in out Window_State; 
+                           Index_To_Increment : Positive;
+                           Random_Index_To_Decrement : Positive)
+     with Pre => Index_To_Decrement <= State.M and 
+                 State.Freqs(Random_Index_To_Decrement) > 0;
+end ISW;
